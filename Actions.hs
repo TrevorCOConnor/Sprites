5f72933c3882cb3da4caa7dc19bfc58dd3f0d73a
Trevor <trevorcoconnor@gmail.com>
2021-01-27 23:40:52 -0500

    Updated shapes to make cone that projects lines that can be obstructed.

--- Actions.hs
+++ Actions.hs
@@ -2,6 +2,7 @@ module Actions where
 
 import Core
 import Text.Read
+import Shapes
 
 dist :: Position -> Position -> Int
 dist (a, b) (x, y) = round $ sqrt ( radicand )
@@ -53,42 +54,29 @@ selectAndValidateTarget bSprite tType board = do
        then return t
        else selectAndValidateTarget bSprite tType board
 
-chooseTargets2 :: BattleSprite -> TargetType -> Board -> IO [Target]
-chooseTargets2 bSprite Self         board = return []
-chooseTargets2 bSprite t@(Emit _)   board = fmap (:[]) (selectAndValidateTarget bSprite t board)
-chooseTargets2 bSprite (Area _ 0)   board = return []
-chooseTargets2 bSprite t@(Area _ n) board = fmap (:[]) (selectAndValidateTarget bSprite t board)
-chooseTargets2 bSprite t@(Selection v 0 r) board = return []
-chooseTargets2 bSprite t@(Selection v n r) board = do
+generateTargets :: BattleSprite -> TargetType -> Board -> [Target]
+generateTargets bSprite Self _ = [battlePosition bSprite]
+
+chooseTargets :: BattleSprite -> TargetType -> Board -> IO [Target]
+chooseTargets bSprite Self         board = return []
+chooseTargets bSprite t@(Emit _)   board = fmap (:[]) (selectAndValidateTarget bSprite t board)
+chooseTargets bSprite (Area _ 0)   board = return []
+chooseTargets bSprite t@(Area _ n) board = fmap (:[]) (selectAndValidateTarget bSprite t board)
+chooseTargets bSprite t@(Selection v 0 r) board = return []
+chooseTargets bSprite t@(Selection v n r) board = do
     t  <- selectAndValidateTarget bSprite t board
-    ts <- chooseTargets2 bSprite (Selection v (n-1) r) board
+    ts <- chooseTargets bSprite (Selection v (n-1) r) board
     return (t:ts)
     
-chooseTargets :: TargetType -> IO [Target]
-chooseTargets Self = return []
-chooseTargets (Emit _) = fmap (:[]) selectTarget 
-chooseTargets (Area _ 0) = return []
-chooseTargets (Area _ n) = fmap (:[]) selectTarget
-chooseTargets (Selection v 0 r) = do return [] 
-chooseTargets (Selection v n r) = do 
-    t <- selectTarget
-    ts <- chooseTargets (Selection v (n-1) r)
-    return (t : ts)
-
-getAndDisplayTargets :: TargetType -> IO ()
-getAndDisplayTargets tType = do
-    targets <- chooseTargets tType
-    foldr (\x y -> do (putStrLn . show) x; y) (putStr "") targets
-
-useAction :: (SpriteActions -> Maybe Action) -> BattleSprite -> Board -> IO Board
+useAction :: (SpriteActions -> Maybe Action) -> BattleSprite -> Board -> IO (BattleSprite, Board)
 useAction f bSprite board = do
     let Just action  = f $ battleActions bSprite
     let tType   = actTarget action
     let effects = actEffects action
     putStr . show $ paintValidTargets bSprite action board
-    targets <- chooseTargets tType
+    targets <- chooseTargets bSprite tType board
     let f eff = eff targets bSprite
-    return $ foldr f board effects  
+    return $ (bSprite, foldr f board effects)
 
 fly :: Action
 fly = Action { actName    = "Fly"
--- Shapes.hs
+++ Shapes.hs
@@ -1,20 +1,83 @@
-import Board
+module Shapes where
+
+import Core
 
 data Shape = Circle | Square | Triangle
 
-type Origin = Position
-type Radius = Int
+type Start  = Position
 type Size   = Int
+type Point  = (Float, Float)
+type Angle  = Float
 
-createCircle :: Origin -> Radius -> [Position] 
-createCircle (x, y) 0 = [(x, y)]
-createCircle (x, y) r = [(c t, s t) | t <- [0.0, step .. 2.0] ] ++ createCircle (x, y) (r-1)
-        where r' = fromIntegral r :: Float
-              c t = x + (round $ r' * ( cos $ t * pi))
-              s t = y + (round $ r' * ( sin $ t * pi))
-              step = 0.01
+d1 :: Position -> Position -> Int
+d1 (a, b) (x, y) = max (abs (x-a)) (abs (y-b))
 
-createSquare :: Origin -> Size -> [Position]
-createSquare (x, y) 0 = [(x,y)]
-createSquare (x, y) s = [(x + tx, y + ty) | tx <- seq, ty <- seq]
-    where seq = [(-s), (1-s) .. s]
+d2 :: Position -> Position -> Float
+d2 p1 p2 = sqrt $ (x-a)^2 + (y-b)^2
+    where (a, b) = floatPosition p1
+          (x, y) = floatPosition p2
+
+pointToDirection :: Position -> Position -> Direction
+pointToDirection (a, b) (x, y) | (x - a) > 0 = East
+                               | (x - a) < 0 = West
+                               | (y - b) > 0 = North
+                               | (y - b) < 0 = South
+directionToAngles :: Direction -> Point
+directionToAngles South = (pi/4,      (3*pi)/4)
+directionToAngles East  = (-pi/4,     pi/4)
+directionToAngles North = (-(3*pi)/4, -pi/4)
+directionToAngles West  = ((3*pi)/4,  (5*pi)/4)
+
+subtractPoints :: Point -> Point -> Point
+subtractPoints p1 p2 = addPoints p1 $ scalePoint (-1.0) p2
+
+addPoints :: Point -> Point -> Point
+addPoints (a, b) (x, y) = (a + x, b + y)
+
+scalePoint :: Float -> Point -> Point
+scalePoint a (x, y) = (a*x, a*y)
+
+floatPosition :: Position -> Point
+floatPosition (x, y) = (x', y')
+    where x' = fromIntegral x :: Float
+          y' = fromIntegral y :: Float
+
+roundPoint :: Point -> Position
+roundPoint (x, y) = (round x, round y)
+
+line :: Position -> Position -> [Position]
+line p1 p2 = map (roundPoint . lineEq) ts
+    where ts = [0.0, 0.1 .. 1.0]
+          lineEq t = addPoints p1' $ scalePoint t $ subtractPoints p2' p1'
+          p1' = floatPosition p1
+          p2' = floatPosition p2
+
+angle :: Position -> Position -> Float
+angle p1@(a, b) p2@(x, y) = acos $ x' / radius
+    where radius = d2 p1 p2
+          x' = fromIntegral (x-a) :: Float
+
+polarPoint :: Start -> Radius -> Angle -> Position
+polarPoint start r angle = roundPoint $ (x, y)
+    where (a, b) = floatPosition start 
+          radius = fromIntegral r :: Float
+          x = (radius * cos angle) + a
+          y = (radius * sin angle) + b
+
+plotLine :: Position -> Board -> Position -> [Position]
+plotLine p1 board p2 = plotPoints ls board
+    where (l:ls) = line p1 p2
+          plotPoints []     b = []
+          plotPoints (l:ls) b = 
+              case o of
+                Vacant          -> l:plotPoints ls b
+                ContainObject   -> []
+                ContainSprite _ -> [l]
+              where o = occupant $ getSquare l b
+
+createCone :: Start -> Direction -> Radius -> Board -> [Position]
+createCone start dir r board = 
+    concat $ map (plotLine start board) $ map (polarPoint start r) angles
+    where (startAngle, endAngle) = directionToAngles dir
+          angles = [startAngle, startAngle + (1/50) .. endAngle]
+
