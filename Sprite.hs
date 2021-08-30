175464477c1bcd5aade3417fc18de2f5e9079127
Trevor <trevorcoconnor@gmail.com>
2021-08-30 01:33:03 -0400

    Added up to sprite movements and return bools from movement functions to indicate success.

--- Sprite.hs
+++ Sprite.hs
@@ -1,5 +1,7 @@
 module Sprite where
 
+import Control.Concurrent.MVar
+
 
 type Position = (Int, Int)
 
@@ -32,6 +34,53 @@ data Sprite = Sprite { sprName :: String
                      }
 
 
-data SpriteContainer = SpriteContainer { sprPosition :: Position
-                                       , sprite :: String
+data SpriteContainer_ = SpriteContainer_ { sprPosition :: Maybe Position
+                                         , sprite :: Sprite
+                                         }
+
+
+data SpriteContainer = SpriteContainer (MVar SpriteContainer_)
+
+-- Query Functions
+extractInnerContainer :: SpriteContainer -> MVar SpriteContainer_
+extractInnerContainer (SpriteContainer innerContainer) = innerContainer
+
+
+extractSpritePosition :: SpriteContainer -> IO (Maybe Position)
+extractSpritePosition (SpriteContainer innerContainer) = do
+    sprCon <- readMVar innerContainer
+    return $ sprPosition sprCon
+
+
+-- Manipulate Functions
+newSpriteContainer :: Sprite -> Maybe Position -> IO SpriteContainer
+newSpriteContainer sprite mpos = newMVar innerContainer >>= return . SpriteContainer
+    where innerContainer = SpriteContainer_ { sprPosition=mpos 
+                                             , sprite=sprite
+                                             }
+
+
+updateSpritePosition :: SpriteContainer -> Position -> IO ()
+updateSpritePosition (SpriteContainer innerContainer) pos = 
+    modifyMVar_ innerContainer (return . updateSpritePosition_ pos)
+
+
+updateSpritePosition_ :: Position -> SpriteContainer_ -> SpriteContainer_
+updateSpritePosition_ pos sprCon = sprCon {sprPosition=(Just pos)}
+
+
+
+-- Example Sprite
+dummy :: Sprite
+dummy = Sprite { sprName="Dummy"
+               , sprType=Fire
+               , sprAttributes=dummyAttributes
+               }
+    where dummyAttributes = Attributes { healthPoints=HealthPoints 10
+                                       , speed=Speed 10
+                                       , endurance=Endurance 10
+                                       , phyiscalAttack=PhysicalAttack 10
+                                       , magicalAttack=MagicalAttack 10
+                                       , physicalDefense=PhysicalDefense 10
+                                       , magicalDefense=MagicalDefense 10
                                        }
